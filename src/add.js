62d0ed0822871a020a968078feb1e2c10f95ed2b
Arjun Chib <arjunpatrickchib@gmail.com>
2020-07-09 18:13:38 -0400

    Catch errors in add command

--- src/add.js
+++ src/add.js
@@ -7,52 +7,50 @@ import lookup from "./lookup.js";
 
 export default async function ({ msg, args }) {
   const [sourceURL, start, end, name, ...aliases] = args;
-  const meme = {
-    name,
-    createdAt: new Date(),
-    author: {
-      name: msg.author.username,
-      id: msg.author.id,
-    },
-    sourceURL,
-    start,
-    end,
-    volume: 1,
-    aliases,
-    tags: [],
-  };
-  memes.push(meme);
-  lookup.set(name, name);
-  aliases.forEach((alias) => lookup.set(alias, name));
-  await fs.promises.writeFile(
-    `./memes/${meme.name}.json`,
-    JSON.stringify(meme, null, 2)
-  );
-  await fs.promises.writeFile(`./lookup.json`, JSON.stringify([...lookup]));
-  await info({ msg, meme });
-
-  const input = ytdl(sourceURL);
-  const transcoder = new prism.FFmpeg({
-    args: [
-      "-analyzeduration",
-      "0",
-      "-loglevel",
-      "0",
-      /*"-f",
-      "s16le",
-      "-ar",
-      "48000",
-      "-ac",
-      "2",*/
-      "-filter:a",
-      "loudnorm",
-      "-c:a",
-      "libopus",
-      "-f",
-      "ogg",
-    ],
-  });
-  const file = fs.createWriteStream(`./audio/${name}.opus`);
-
-  input.pipe(transcoder).pipe(file);
+  const audioPath = `./audio/${name}.opus`;
+  const memePath = `./memes/${name}.json`;
+  try {
+    const input = ytdl(sourceURL);
+    const transcoder = new prism.FFmpeg({
+      // prettier-ignore
+      args: [
+      "-analyzeduration", "0",
+      "-loglevel", "0",
+      "-ss", start,
+      "-t", end,
+      "-async", "1",
+      "-filter:a", "loudnorm",
+      "-c:a", "libopus",
+      "-f", "ogg",
+    ]
+    });
+    const file = fs.createWriteStream(audioPath);
+    input.pipe(transcoder).pipe(file);
+    const meme = {
+      name,
+      createdAt: new Date(),
+      author: {
+        name: msg.author.username,
+        id: msg.author.id,
+      },
+      sourceURL,
+      start,
+      end,
+      volume: 1,
+      aliases,
+      tags: [],
+    };
+    memes.push(meme);
+    [name, ...aliases].forEach((cmd) => lookup.set(cmd, name));
+    await fs.promises.writeFile(memePath, JSON.stringify(meme, null, 2));
+    await fs.promises.writeFile(`./lookup.json`, JSON.stringify([...lookup]));
+    await info({ msg, meme });
+  } catch (e) {
+    if (await fs.promises.access(audioPath)) await fs.unlink(audioPath);
+    if (await fs.promises.access(memePath)) await fs.unlink(memePath);
+    const memeIndex = memes.findIndex((meme) => meme.name);
+    if (memeIndex !== -1) memes.splice(memeIndex, 1);
+    [name, ...aliases].forEach((cmd) => lookup.delete(cmd, name));
+    throw e;
+  }
 }
