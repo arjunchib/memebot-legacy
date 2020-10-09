579e62df405312adccb5d841c43aec14b3972933
Arjun Chib <arjunpatrickchib@gmail.com>
2020-10-09 17:56:06 -0400

    Update list command to only display at most 60 memes

--- src/list/index.js
+++ src/list/index.js
@@ -3,12 +3,12 @@ import sort from "./sort.js";
 import Meme from "../structures/Meme.js";
 import segment from "../util/segment.js";
 
-const MAX_SEGMENTS = 60;
+const MAX_MEMES = 60;
 
 export default async function ({ msg, args, arg }) {
   const field = arg.length === 2 ? arg[1] : undefined;
   let myMemes = [...new Set(Meme.all.values())];
-  if (args.length === 0) args = ["limit:60", "sort:shuffle"];
+  if (args.length === 0) args = ["sort:shuffle"];
   args.forEach((arg) => {
     const listArgs = arg.split(":");
     switch (listArgs[0]) {
@@ -18,14 +18,9 @@ export default async function ({ msg, args, arg }) {
       case "sort":
         myMemes = sort(myMemes, listArgs);
         break;
-      case "limit":
-        myMemes = myMemes.slice(0, listArgs[1]);
-        break;
     }
   });
+  myMemes = myMemes.slice(0, MAX_MEMES);
   const segments = myMemes.map((meme) => segment(meme.name, meme[field]));
-  const maxLength = segments[0].length * MAX_SEGMENTS + MAX_SEGMENTS - 1;
-  await msg.channel.send(segments.join(" "), {
-    split: { char: " ", maxLength },
-  });
+  await msg.channel.send(segments.join(" "));
 }
