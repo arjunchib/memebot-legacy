2a047be74427ab6d9c08d904793031b67e27b234
Arjun Chib <arjunpatrickchib@gmail.com>
2020-12-01 17:37:25 -0500

    Remove private property on info command for public

--- src/meme/info.js
+++ src/meme/info.js
@@ -3,6 +3,7 @@ import access from "../util/access.js";
 export default async function ({ msg, meme }) {
   const data = { ...meme.toJSON() };
   if (!access(msg, 1)) delete data.author;
+  if (!access(msg, 1)) delete data.private;
   const output = JSON.stringify(data, null, 2);
   await msg.channel.send(output, { code: "json" });
 }
